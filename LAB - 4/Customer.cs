ae5cd66e281ec18b2db6b24f2445e219943daa67
Meet Indoriya <meetindoriya5@gmail.com>
2025-12-20 16:41:15 +0530

    Add files via upload

---
+++ LAB - 4/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PRACTICALS_OOP
+{
+    public class Customer
+    {
+        public virtual void CalculateBill(double Amount) { 
+        }
+
+    }
+
+    public class RegularCustomer : Customer
+    {
+        double Amount;
+        public override void CalculateBill(double Amount)
+        {
+            this.Amount = Amount;
+            double Discount = 0; 
+            Discount = Discount + (Amount * 0.08);
+            Console.WriteLine("\nRegularCustomer Discount:");
+            Console.WriteLine("Orignal Price : " + Amount + " Discount: 8% Discounted Price:" + Discount + " Grand Price: " + (Amount - Discount));
+        }
+    }
+
+    public class PremiumCustomer : Customer
+    {
+        double Amount;
+        public override void CalculateBill(double Amount)
+        {
+            this.Amount = Amount;
+            double Discount = 0;
+            Discount = Discount + (Amount * 0.12);
+            Console.WriteLine("\nPremiumCustomer Discount: \n\n");
+            Console.WriteLine("Orignal Price : " + Amount + " Discount: 12% Discounted Price:" + Discount + " Grand Price: " + (Amount - Discount));
+        }
+    }
+}
---
+++ LAB - 4/Library.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PRACTICALS_OOP
+{
+    public class Library
+    {
+        protected string authorName;
+        protected string Title;
+
+        public virtual void library_displatInfo()
+        {
+
+        }
+    }
+
+    public class Book : Library
+    {
+        public void setBookDetail(string Title, string authorName)
+        {
+            this.authorName = authorName;
+            this.Title = Title;
+        }
+        public override void library_displatInfo()
+        {
+            Console.WriteLine("Book Title : " + Title + " Written By:  " + authorName);
+        }
+    }
+
+    public class Magazine : Library
+    {
+        public void setMagazineDetail(string Title, string authorName)
+        {
+            this.authorName = authorName;
+            this.Title = Title;
+        }
+        public override void library_displatInfo()
+        {
+            Console.WriteLine("Magazine Title : " + Title + " Written By:  " + authorName);
+        }
+    }
+}
